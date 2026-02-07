312632dd954542a121993976c7588687d7bf8537
KaranBhaskar <karanbhaskar014@gmail.com>
2026-02-07 01:16:24 -0500

    Revert "K: scanner"
    This reverts commit 015885d8d3d3fec13be9b465b2c4a741f67b0385.

--- scripts/scan.sh
+++ scripts/scan.sh
@@ -1,15 +1,1 @@
 # TODO: write the script
-
-echo "\n====== SCANNING FOR CREDIT CARD NUMBERS ======"
-grep -rE --color=always '\b([0-9]{4}[- ]){3}[0-9]{4}\b' . --exclude-dir={.git} --line-number
-echo "ニニニニニニニ== CREDIT CARD SCAN COMPLETE ニニニニニニニニニ"
-
-echo "\n===== SCANNING FOR Social Insurance NUMBERS ======"
-grep -rE --color=always '\b[0-9]{3}[-][0-9]{2}-[0-9]{4}\b' . --exclude-dir={.git} --line-number
-echo "ニニニニニニニ== Social Inurance SCAN COMPLETE ニニニニニニニニニ"
-
-
-echo "\n===== SCANNING FOR PHONE NUMBERS ======"
-grep -rE --color=always '\b[0-9]{3}[-][0-9]{3}-[0-9]{4}\b' . --exclude-dir={.git} --line-number
-grep -rE --color=always '\b[0-9]{3} [0-9]{3}-[0-9]{4}\b' . --exclude-dir={.git} --line-number
-echo "ニニニニニニニ== PHONE NUMBER SCAN COMPLETE ニニニニニニニニニ"
